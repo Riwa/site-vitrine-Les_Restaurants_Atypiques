5c801ea7c82b18004e85ed9c782684b219ac406c
Collet Lauriane <lauriane.collet1@gmail.com>
2017-03-07 00:37:04 +0100

    I have change the path of images

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   $('.slideshowBkg').vegas({
     slides: [
-      { src: 'images/01.jpg' },
-      { src: 'images/02.jpg' },
+      { src: '../images/01.jpg' },
+      { src: '../images/02.jpg' },
     ],
   });
 });
