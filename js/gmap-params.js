cbe862ead52afcd5802e4ab2a947bdc5c4515c54
Arthur Alloncle <arthuralloncle@gmail.com>
2017-03-07 11:56:42 +0100

    WIP google map

--- js/gmap-params.js
+++ js/gmap-params.js
@@ -1,5 +1,392 @@
 $(document).ready(function () {
 
+	let screenWidth = window.innerWidth;
+	console.log(screenWidth);
+
+	let style = [
+		{
+			"featureType": "administrative",
+			"elementType": "labels.text.fill",
+			"stylers": [
+				{
+					"color": "#444444"
+				}
+			]
+		},
+		{
+			"featureType": "administrative.country",
+			"elementType": "geometry.fill",
+			"stylers": [
+				{
+					"color": "#ff0000"
+				}
+			]
+		},
+		{
+			"featureType": "administrative.country",
+			"elementType": "geometry.stroke",
+			"stylers": [
+				{
+					"color": "#ffffff"
+				}
+			]
+		},
+		{
+			"featureType": "administrative.province",
+			"elementType": "geometry.stroke",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "administrative.province",
+			"elementType": "labels.text",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "administrative.locality",
+			"elementType": "geometry.stroke",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "administrative.locality",
+			"elementType": "labels.text",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "administrative.locality",
+			"elementType": "labels.text.stroke",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "administrative.neighborhood",
+			"elementType": "labels",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "landscape",
+			"elementType": "all",
+			"stylers": [
+				{
+					"color": "#f2f2f2"
+				}
+			]
+		},
+		{
+			"featureType": "landscape.man_made",
+			"elementType": "geometry.fill",
+			"stylers": [
+				{
+					"color": "#f7a50b"
+				},
+				{
+					"visibility": "on"
+				}
+			]
+		},
+		{
+			"featureType": "landscape.man_made",
+			"elementType": "geometry.stroke",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "landscape.man_made",
+			"elementType": "labels.text",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "landscape.man_made",
+			"elementType": "labels.text.fill",
+			"stylers": [
+				{
+					"color": "#ff0000"
+				},
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "landscape.man_made",
+			"elementType": "labels.icon",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "landscape.natural",
+			"elementType": "geometry.fill",
+			"stylers": [
+				{
+					"color": "#da8900"
+				}
+			]
+		},
+		{
+			"featureType": "landscape.natural",
+			"elementType": "labels.text",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "poi",
+			"elementType": "all",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "poi",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "poi",
+			"elementType": "labels",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "poi",
+			"elementType": "labels.text",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "road",
+			"elementType": "all",
+			"stylers": [
+				{
+					"saturation": -100
+				},
+				{
+					"lightness": 45
+				}
+			]
+		},
+		{
+			"featureType": "road",
+			"elementType": "geometry.fill",
+			"stylers": [
+				{
+					"color": "#ff8a00"
+				}
+			]
+		},
+		{
+			"featureType": "road",
+			"elementType": "geometry.stroke",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "road",
+			"elementType": "labels",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "road",
+			"elementType": "labels.text",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "road",
+			"elementType": "labels.text.fill",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "road",
+			"elementType": "labels.text.stroke",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "road",
+			"elementType": "labels.icon",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "all",
+			"stylers": [
+				{
+					"visibility": "simplified"
+				}
+			]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "geometry.fill",
+			"stylers": [
+				{
+					"visibility": "on"
+				}
+			]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "geometry.stroke",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "labels.text",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "labels.icon",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "road.arterial",
+			"elementType": "labels.icon",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "transit",
+			"elementType": "all",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "transit",
+			"elementType": "labels.icon",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "water",
+			"elementType": "all",
+			"stylers": [
+				{
+					"color": "#f2ba40"
+				},
+				{
+					"visibility": "on"
+				}
+			]
+		},
+		{
+			"featureType": "water",
+			"elementType": "geometry.fill",
+			"stylers": [
+				{
+					"color": "#ffffff"
+				}
+			]
+		},
+		{
+			"featureType": "water",
+			"elementType": "labels.text",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "water",
+			"elementType": "labels.icon",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		}
+	]
+
 	let lyon = {
 		nom: 'Restaurant Atypique Lyon',
 		adresse: '17, rue Delandine <br> 69007 Lyon',
@@ -20,237 +407,51 @@ $(document).ready(function () {
 		div: '#map',
 		lat: 45.74627029999999,
 		lng: 4.826846799999998,
-		zoom: 5
+		zoom: 5,
+		styles: style
+
 	});
 
-	let style = [
-		{
-			"elementType": "labels",
-			"stylers": [
-				{
-					"visibility": "off"
-				},
-				{
-					"color": "#f49f53"
-				}
-			]
-		},
-		{
-			"featureType": "landscape",
-			"stylers": [
-				{
-					"color": "#f9ddc5"
-				},
-				{
-					"lightness": -7
-				}
-			]
-		},
-		{
-			"featureType": "road",
-			"stylers": [
-				{
-					"color": "#813033"
-				},
-				{
-					"lightness": 43
-				}
-			]
-		},
-		{
-			"featureType": "poi.business",
-			"stylers": [
-				{
-					"color": "#645c20"
-				},
-				{
-					"lightness": 38
-				}
-			]
-		},
-		{
-			"featureType": "water",
-			"stylers": [
-				{
-					"color": "#1994bf"
-				},
-				{
-					"saturation": -69
-				},
-				{
-					"gamma": 0.99
-				},
-				{
-					"lightness": 43
-				}
-			]
-		},
-		{
-			"featureType": "road.local",
-			"elementType": "geometry.fill",
-			"stylers": [
-				{
-					"color": "#f19f53"
-				},
-				{
-					"weight": 1.3
-				},
-				{
-					"visibility": "on"
-				},
-				{
-					"lightness": 16
-				}
-			]
-		},
-		{
-			"featureType": "poi.business"
-		},
-		{
-			"featureType": "poi.park",
-			"stylers": [
-				{
-					"color": "#645c20"
-				},
-				{
-					"lightness": 39
-				}
-			]
-		},
-		{
-			"featureType": "poi.school",
-			"stylers": [
-				{
-					"color": "#a95521"
-				},
-				{
-					"lightness": 35
-				}
-			]
-		},
-		{},
-		{
-			"featureType": "poi.medical",
-			"elementType": "geometry.fill",
-			"stylers": [
-				{
-					"color": "#813033"
-				},
-				{
-					"lightness": 38
-				},
-				{
-					"visibility": "off"
-				}
-			]
-		},
-		{},
-		{},
-		{},
-		{},
-		{},
-		{},
-		{},
-		{},
-		{},
-		{},
-		{},
-		{
-			"elementType": "labels"
-		},
-		{
-			"featureType": "poi.sports_complex",
-			"stylers": [
-				{
-					"color": "#9e5916"
-				},
-				{
-					"lightness": 32
-				}
-			]
-		},
-		{},
-		{
-			"featureType": "poi.government",
-			"stylers": [
-				{
-					"color": "#9e5916"
-				},
-				{
-					"lightness": 46
-				}
-			]
-		},
-		{
-			"featureType": "transit.station",
-			"stylers": [
-				{
-					"visibility": "off"
-				}
-			]
-		},
-		{
-			"featureType": "transit.line",
-			"stylers": [
-				{
-					"color": "#813033"
-				},
-				{
-					"lightness": 22
-				}
-			]
-		},
-		{
-			"featureType": "transit",
-			"stylers": [
-				{
-					"lightness": 38
-				}
-			]
-		},
-		{
-			"featureType": "road.local",
-			"elementType": "geometry.stroke",
-			"stylers": [
-				{
-					"color": "#f19f53"
-				},
-				{
-					"lightness": -10
-				}
-			]
-		},
-		{},
-		{},
-		{}
-	];
 
-	map.setOptions({ style: style })
+	//	map.setOptions({ style: style })
 
-	map.addMarker({
-		lat: 45.74627029999999,
-		lng: 4.826846799999998,
-		title: 'Restaurant Atypique Lyon',
-		// OUVERTURE DE LA FICHE RESTAURANT
-		click: function (e) {
-			$('.ficheRestaurant')
-				.removeClass('fadeOutDown')
-				.addClass('opened fadeInUp');
-
-			$('#ficheClose').click(function () {
+	if (screenWidth < 768) {
+		map.addMarker({
+			lat: 45.74627029999999,
+			lng: 4.826846799999998,
+			title: 'Restaurant Atypique Lyon',
+			// OUVERTURE DE LA FICHE RESTAURANT
+			click: function (e) {
 				$('.ficheRestaurant')
-					.removeClass('fadeInUp')
-					.addClass('fadeOutDown');
-			});
+					.removeClass('fadeOutDown')
+					.addClass('opened fadeInUp');
 
-			$('.ficheRestaurant h3').text(lyon.nom);
-			$('.restaurantDescription').html(lyon.description);
-			$('.restaurantAdresse').html(lyon.adresse);
-			$('.restaurantHoraire').html(lyon.horaires);
-			$('.restaurantContact').html(lyon.contact);
-		}
-		// FIN DE FONCTION OUVERTURE FICHE RESTAURANT
-	});
+				$('#ficheClose').click(function () {
+					$('.ficheRestaurant')
+						.removeClass('fadeInUp')
+						.addClass('fadeOutDown');
+				});
+
+				$('.ficheRestaurant h3').text(lyon.nom);
+				$('.restaurantDescription').html(lyon.description);
+				$('.restaurantAdresse').html(lyon.adresse);
+				$('.restaurantHoraire').html(lyon.horaires);
+				$('.restaurantContact').html(lyon.contact);
+			},
+
+			// FIN DE FONCTION OUVERTURE FICHE RESTAURANT
+		});
+	} else {
+		map.addMarker({
+			lat: 45.74627029999999,
+			lng: 4.826846799999998,
+			title: 'Restaurant Atypique Lyon',
+			// AFFICHAGE D'UNE TOOLTIP SUR ECRAN PORTABLE
+			infoWindow: {
+				content: '<h3>' + lyon.nom + '</h3><p>' + lyon.description + '</p><p>' + lyon.adresse + '</p><p>' + lyon.horaires + '</p><p>' + lyon.contact + '</p>'
+			},
+		});
+	}
 
 	map.addMarker({
 		lat: 48.473369,
@@ -271,7 +472,8 @@ $(document).ready(function () {
 			$('.restaurantAdresse').html(laLoupe.adresse);
 			$('.restaurantHoraire').html(laLoupe.horaires);
 			$('.restaurantContact').html(laLoupe.contact);
-		}
+		},
+
 		// FIN DE FONCTION OUVERTURE FICHE RESTAURANT
 	});
 });
