a0bea37e2dc8e7bdf904db05e35c2924f5a10b88
Arthur Alloncle <arthuralloncle@gmail.com>
2017-03-09 19:55:41 +0100

    General fixes

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,6 +3,9 @@ $(document).ready(function () {
     slides: [
       { src: '../images/01.jpg' },
       { src: '../images/02.jpg' },
+      { src: '../images/image_autrecontact.jpg' },
+      { src: '../images/image_contact.jpg' },
+      { src: '../images/image_devenir_franchise.jpg' },
     ],
   });
 });
